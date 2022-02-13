16f5707c74877de2e9c0703184c3a5bda63826a2
S <s.1gn.sgrfu@gmail.com>
2022-02-13 13:49:04 +0900

    add:time info

--- musicplayer.py
+++ musicplayer.py
@@ -14,14 +14,14 @@ import time
 import threading
 import os
 
-plf=0
+
 
 def plays():
     sound=AudioSegment.from_file(sud,format=ext,bitrate=bit)
     play(sound)
 
 def info():
-    print('Title:',ti1,'\tFormat:',ext,'\tBitrate:',bit//1000,'Kbps\n')
+    print('Title:',ti1,'\tFormat:',ext,'\ttime:',round(time1),'sec','\tBitrate:',bit//1000,'Kbps\n')
 
 def playing():
     while True:
@@ -39,12 +39,15 @@ def playing():
         time.sleep(1)
     #os.system('cls')
 
+plf=0
+
 thread1=threading.Thread(target=plays)
 thread2=threading.Thread(target=info)
 thread3=threading.Thread(target=playing)
 
 sud = 'C:\Windows\Media\Ring10.wav'
 bit = int(mediainfo(sud)['bit_rate'])
+
 
 target='\\'
 idx=sud[::-1].find(target)
@@ -54,6 +57,9 @@ target2='.'
 idx2=ti.find(target2)
 ext=ti[idx2+1:]
 ti1=ti[:idx2]
+
+sau = AudioSegment.from_file(sud, ext)
+time1 = sau.duration_seconds
 
 
 thread1.start()
