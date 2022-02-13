5d20d32fc51066d4c5e803ea12b504079008c6a8
S <s.1gn.sgrfu@gmail.com>
2022-02-13 12:26:53 +0900

    first

---
+++ musicplayer.py
@@ -0,0 +1,60 @@
+from pydub import AudioSegment
+from pydub.utils import mediainfo
+from pydub.playback import play
+import time
+import threading
+import os
+
+plf=0
+
+def plays():
+    sound=AudioSegment.from_file(sud,format=ext,bitrate=bit)
+    play(sound)
+
+def info():
+    print('Title:',ti1,'\tFormat:',ext,'\tBitrate:',bit//1000,'Kbps\n')
+
+def playing():
+    while True:
+        print('\rPlaying   ',end='')
+        if plf==1:break
+        time.sleep(1)
+        print('\rPlaying.  ',end='')
+        if plf==1:break
+        time.sleep(1)
+        print('\rPlaying.. ',end='')
+        if plf==1:break
+        time.sleep(1)
+        print('\rPlaying...',end='')
+        if plf==1:break
+        time.sleep(1)
+    #os.system('cls')
+
+thread1=threading.Thread(target=plays)
+thread2=threading.Thread(target=info)
+thread3=threading.Thread(target=playing)
+
+sud = 'C:\Windows\Media\Ring10.wav'
+bit = int(mediainfo(sud)['bit_rate'])
+
+target='\\'
+idx=sud[::-1].find(target)
+ti=sud[-idx:]
+
+target2='.'
+idx2=ti.find(target2)
+ext=ti[idx2+1:]
+ti1=ti[:idx2]
+
+
+thread1.start()
+thread2.start()
+
+thread2.join()
+thread3.start()
+
+thread1.join()
+plf=1
+thread3.join()
+
+print('\nBye')
